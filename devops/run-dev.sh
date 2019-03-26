3be4701bcf2ec6fc9581d372dd9bb260ffbfaab2
Stevo Dlugolinsky <stevo@dlugolinsky.com>
2019-03-26 15:25:48 +0100

    developer image

--- devops/run-dev.sh
+++ devops/run-dev.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 docker run\
- --name=deep-mods\
+ --name=deep-oc-mods-dev\
  --rm\
  -it\
- -p 5000:5000\
- -v ${HOME}/.config:/root/.config\
- -v $(HOME)/.oidc-agent:/root/.oidc-agent\
- -v $(HOME)/work/DEEP/mods:/srv/mods\
- deephdc/deep-oc-mods-dev\
+ -p 5001:5000\
+ -v ${HOME}/.config/rclone:/root/.config/rclone\
+ -v "$(pwd)"/../../mods:/srv/mods\
+ -v "$(pwd)"/../../deepaas:/srv/deepaas\
+ deep-oc-mods:dev\
  /bin/bash
