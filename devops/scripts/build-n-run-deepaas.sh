6f7a1f938f03feb7a632baca73d09722ce6c256d
Stevo Dlugolinsky <stevo@dlugolinsky.com>
2019-07-11 19:10:17 +0200

    updated run script for test

--- devops/scripts/build-n-run-deepaas.sh
+++ devops/scripts/build-n-run-deepaas.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # This is a helper script, which automatically installs mods and deepaas. After a successfull build, it executes deepaas.
-cd mods/ && \
-pip3 install -e . && \
-cd .. && \
 cd deepaas/ && \
 pip3 install -U . && \
 cd .. && \
-deepaas-run --listen-ip 0.0.0.0
+cd mods/ && \
+pip3 install -e . && \
+cd .. && \
+deepaas-run --openwhisk-detect --listen-ip 0.0.0.0 --listen-port 5000
