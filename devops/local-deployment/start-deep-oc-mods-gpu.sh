089d5fbe2fa45b3f9f1732b9732f4e300245be1a
Stefan Dlugolinsky <stefan.dlugolinsky@savba.sk>
2019-10-23 14:40:43 +0000

    local deployment update

--- devops/local-deployment/start-deep-oc-mods-gpu.sh
+++ devops/local-deployment/start-deep-oc-mods-gpu.sh
@@ -8,10 +8,10 @@ docker run\
   --rm\
   --name deep-oc-mods-gpu\
   -p 5000:5000\
-  -v "$(pwd)"/volumes/deep-oc-mods/.config/:/root/.config\
-  -v "$(pwd)"/volumes/deep-oc-mods/log:/srv/deep-debug_log/log\
-  -v "$(pwd)"/volumes/deep-oc-mods/data:/srv/mods/data\
-  -v "$(pwd)"/volumes/deep-oc-mods/models:/srv/mods/models\
-  -v "$(pwd)"/volumes/deep-oc-mods/checkpoints:/srv/mods/checkpoints/\
+  -v "$(pwd)"/volume/.config/:/root/.config\
+  -v "$(pwd)"/volume/log/:/srv/deep-debug_log/log\
+  -v "$(pwd)"/volume/data/:/srv/mods/data\
+  -v "$(pwd)"/volume/models/:/srv/mods/models\
+  -v "$(pwd)"/volume/checkpoints/:/srv/mods/checkpoints\
   deephdc/deep-oc-mods:gpu\
   deepaas-run --openwhisk-detect --listen-ip 0.0.0.0 --listen-port=$PORT0
