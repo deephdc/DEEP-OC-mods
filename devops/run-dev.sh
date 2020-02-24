cdb5360c474bf87748d0ae6e2f7d5cf4aea6a861
Stevo Dlugolinsky <stevo@dlugolinsky.com>
2020-02-24 15:03:07 +0000

    update

--- devops/run-dev.sh
+++ devops/run-dev.sh
@@ -1,5 +1,18 @@
 #!/bin/bash
+
+if [[ -z $ONECLIENT_ACCESS_TOKEN ]] || [[ -z $ONECLIENT_PROVIDER_HOST ]]; then
+	echo \
+"""
+Please set the environment variables:
+
+export ONECLIENT_ACCESS_TOKEN=
+export ONECLIENT_PROVIDER_HOST=
+"""
+	exit
+fi
+
 docker run\
+ --privileged\
  --name=deep-oc-mods-dev\
  --rm\
  -it\
@@ -7,5 +20,8 @@ docker run\
  -v ${HOME}/.config/rclone:/root/.config/rclone\
  -v "$(pwd)"/../../mods:/srv/mods\
  -v "$(pwd)"/../../deepaas:/srv/deepaas\
+ -e ONECLIENT_ACCESS_TOKEN=$ONECLIENT_ACCESS_TOKEN \
+ -e ONECLIENT_PROVIDER_HOST=$ONECLIENT_PROVIDER_HOST \
+ -e APP_INPUT_OUTPUT_BASE_DIR=/mnt/onedata/mods \
  deep-oc-mods:dev\
  /bin/bash
--- run-cpu.sh
+++ run-cpu.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
-docker run\
- --name=deep-mods-cpu\
- --rm\
- -d\
- -p 5000:5000\
- -v "$HOME"/.config:/root/.config\
- deephdc/deep-oc-mods:cpu
+
+if [[ -z $ONECLIENT_ACCESS_TOKEN ]] || [[ -z $ONECLIENT_PROVIDER_HOST ]]; then
+	echo \
+"""
+Please set the environment variables:
+
+export ONECLIENT_ACCESS_TOKEN=
+export ONECLIENT_PROVIDER_HOST=
+"""
+	exit
+fi
+
+docker run \
+ --privileged \
+ --name=deep-mods-cpu \
+ --rm \
+ -it \
+ -p 5000:5000 \
+ -v "$HOME"/.config:/root/.config \
+ -e ONECLIENT_ACCESS_TOKEN=$ONECLIENT_ACCESS_TOKEN \
+ -e ONECLIENT_PROVIDER_HOST=$ONECLIENT_PROVIDER_HOST \
+ -e APP_INPUT_OUTPUT_BASE_DIR=/mnt/onedata/mods \
+ deephdc/deep-oc-mods:cpu \
+ deep-start -do
