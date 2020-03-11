42c26fdff2757cb4e48ea3bb7cdc487d054b65db
Stevo Dlugolinsky <stevo@dlugolinsky.com>
2020-03-11 11:43:20 +0000

    updated run scripts with published ports 6006 and 8888

--- devops/run-dev.sh
+++ devops/run-dev.sh
@@ -11,17 +11,19 @@ export ONECLIENT_PROVIDER_HOST=
 	exit
 fi
 
-docker run\
- --privileged\
- --name=deep-oc-mods-dev\
- --rm\
- -it\
- -p 5000:5000\
- -v ${HOME}/.config/rclone:/root/.config/rclone\
- -v "$(pwd)"/../../mods:/srv/mods\
- -v "$(pwd)"/../../deepaas:/srv/deepaas\
+docker run \
+ --privileged \
+ --name=deep-oc-mods-dev \
+ --rm \
+ -it \
+ -p 5000:5000 \
+ -p 6006:6006 \
+ -p 8888:8888 \
+ -v ${HOME}/.config/rclone:/root/.config/rclone \
+ -v "$(pwd)"/../../mods:/srv/mods \
+ -v "$(pwd)"/../../deepaas:/srv/deepaas \
  -e ONECLIENT_ACCESS_TOKEN=$ONECLIENT_ACCESS_TOKEN \
  -e ONECLIENT_PROVIDER_HOST=$ONECLIENT_PROVIDER_HOST \
  -e APP_INPUT_OUTPUT_BASE_DIR=/mnt/onedata/mods \
- deep-oc-mods:dev\
+ deep-oc-mods:dev \
  /bin/bash
--- run-cpu-test.sh
+++ run-cpu-test.sh
@@ -17,9 +17,11 @@ docker run \
  --rm \
  -d \
  -p 5000:5000 \
+ -p 6006:6006 \
+ -p 8888:8888 \
  -v "$HOME"/.config:/root/.config \
  -e ONECLIENT_ACCESS_TOKEN=$ONECLIENT_ACCESS_TOKEN \
  -e ONECLIENT_PROVIDER_HOST=$ONECLIENT_PROVIDER_HOST \
  -e APP_INPUT_OUTPUT_BASE_DIR=/mnt/onedata/mods \
  deephdc/deep-oc-mods:cpu-test \
- deep-start -do
+ $*
